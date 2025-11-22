df6e80910edd3edee065c14f5b9cd5bea0bbd4f6
neerajv-14 <neerajkumarvitta@gmail.com>
2025-11-22 13:46:24 +0530

    search-functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,7 @@ export const routes: Routes = [
         component: SearchComponent
     },
     {
-        path: "search-result",
+        path: "search-result/:fromId/:toId/:date",
         component: SearchResultComponent
     },
     {
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { Search } from '../../model/model';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -12,18 +13,21 @@ import { FormsModule } from '@angular/forms';
 export class SearchComponent implements OnInit{
 
   http = inject(HttpClient);
+  router = inject(Router);
   locationList: any[] = [];
-  
+  from!: string;
+  to!: string;
+  date!: string;
   searchObj!: Search;
 
   ngOnInit(): void {
-    debugger;
+    //debugger;
       this.getAllBusLocations();
   }
 
   getAllBusLocations()
   {
-    debugger;
+    //debugger;
     this.http.get("https://api.freeprojectapi.com/api/BusBooking/GetBusLocations").subscribe((data:any)=>{
         this.locationList = data;
     });
@@ -31,7 +35,19 @@ export class SearchComponent implements OnInit{
 
 
   searchBus(){
-    this.http.get('https://api.freeprojectapi.com/api/BusBooking/searchBus2?fromLocation=' + this.searchObj.fromLocation  + '&toLocation=' + this.searchObj.toLocation+'&travelDate=' + this.searchObj.date)
+    // this.http.get('https://api.freeprojectapi.com/api/BusBooking/searchBus2?fromLocation=' + this.searchObj.fromLocation  + '&toLocation=' + this.searchObj.toLocation+'&travelDate=' + this.searchObj.date)
+    // .subscribe((data:any)=> {
+
+    // });
+    // this.from = this.searchObj.fromLocation;
+    // this.to = this.searchObj.toLocation;
+    // this.date = this.searchObj.date;
+    this.searchObj.fromLocation = "1";
+    this.searchObj.toLocation = "2";
+    this.searchObj.date = "18-10-2023";
+    //this.router.navigate(['/search-result',this.from, this.to ,this.date]);
+
+    this.router.navigate(['/search-result',this.searchObj.fromLocation, this.searchObj.toLocation ,this.searchObj.date]);
   }
 
 }
