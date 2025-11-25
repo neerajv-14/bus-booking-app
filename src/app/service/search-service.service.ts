f81c2229eee7246cb5e202a40b60b8fd6e3f910f
neerajv-14 <neerajkumarvitta@gmail.com>
2025-11-25 22:54:59 +0530

    seat selection , entries, submit

--- src/app/service/search-service.service.ts
+++ src/app/service/search-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { IScheduleData, Search } from '../model/model';
+import { BusBooking, IScheduleData, Search } from '../model/model';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -16,5 +16,9 @@ export class SearchService {
 
   getBusScheduleById(scheduleId: number) : Observable<IScheduleData> {
     return this.http.get<IScheduleData>('https://api.freeprojectapi.com/api/BusBooking/GetBusScheduleById?id='+scheduleId);
+  }
+
+  postBooking(busbooking: BusBooking){
+    return this.http.post("https://api.freeprojectapi.com/api/BusBooking/PostBusBooking",busbooking);
   }
 }
